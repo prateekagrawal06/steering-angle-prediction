22703b89178e3092ddb3e3d65f190439265ee8da
prateek agrawal <prateekagrawal@prateeks-MacBook-Pro.local>
2018-01-08 23:04:51 -0800

    writing a batch generator and keras model

--- main.py
+++ main.py
@@ -1,38 +1,107 @@
 import csv
 import cv2
 from sklearn.model_selection import train_test_split
+import sklearn
 import numpy as np
+import os
+from keras.models import Sequential
+from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+from keras.layers import Lambda
 
 CSV_FILE_PATH = "/Users/prateekagrawal/Desktop/my_git/udacity/udacity_sim_data/data/driving_log.csv"
 IMAGE_FILE_PATH = "/Users/prateekagrawal/Desktop/my_git/udacity/udacity_sim_data/data"
+BATCH_SIZE = 32
+CORRECTION = 0.2
+AUGMENTATION = True
+IMAGE_SHAPE = []
 
 logs = []
 with open(CSV_FILE_PATH) as csv_file:
     reader = csv.reader(csv_file)
+    next(reader)
+
     for line in reader:
-        logs.append(line)
+
+        if AUGMENTATION:
+
+            log_center = [line[0], float(line[3])]
+            log_left = [line[1].split()[0], float(line[3]) + CORRECTION]
+            log_right = [line[2].split()[0], float(line[3]) - CORRECTION]
+
+            flipped_image_file_name = 'IMG/flipped' + line[0].split('center')[1]
+            flipped_image_full_file_name = IMAGE_FILE_PATH + '/' + flipped_image_file_name
+            if not os.path.isfile(flipped_image_full_file_name):
+                flipped_image = np.fliplr(cv2.imread(IMAGE_FILE_PATH + '/' + line[0]))
+                cv2.imwrite(flipped_image_full_file_name, flipped_image)
+            log_flipped = [flipped_image_file_name, -float(line[3])]
+            logs.extend([log_center, log_left, log_right, log_flipped])
 
 print("total log files found are: ", len(logs))
-
-images = []
-steering_angles = []
-
-for log in logs[1:]:
-    center_image_path = IMAGE_FILE_PATH + '/' + log[0]
-    image = cv2.imread(center_image_path)
-    images.append(image)
-    steering_angles.append(float(log[3]))
-
-X_train,  X_test, y_train, y_test = train_test_split(images, steering_angles, shuffle=True, test_size=0.2)
+training_logs, testing_logs = train_test_split(logs, test_size=0.2, random_state=1234, shuffle=True)
+print("training logs size {}".format(len(training_logs)))
+print("testing logs size {}".format(len(testing_logs)))
 
 
-X_train = np.array(X_train)
-X_test = np.array(X_test)
-y_train = np.array(y_train)
-y_test = np.array(y_test)
+def batch_generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1:  # Loop forever so the generator never terminates
+        sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
 
-print(X_train.shape)
-print(y_train.shape)
-print(X_test.shape)
-print(y_test.shape)
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = IMAGE_FILE_PATH + '/' + batch_sample[0]
+                image = cv2.imread(name)
+                steering_angle = float(batch_sample[1])
+                images.append(image)
+                angles.append(steering_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+
+train_generator = batch_generator(training_logs, BATCH_SIZE)
+test_generator = batch_generator(testing_logs, BATCH_SIZE)
+
+model = Sequential()
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+# first set of CONV => RELU => POOL
+model.add(Convolution2D(6, 5, 5, border_mode="same", input_shape=(160, 320, 3)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+# second set of CONV => RELU => POOL
+model.add(Convolution2D(16, 5, 5, border_mode="same"))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+# set of FC => RELU layers
+model.add(Flatten())
+model.add(Dense(256))
+model.add(Activation("relu"))
+
+model.add(Dense(128))
+model.add(Activation("relu"))
+
+model.add(Dense(64))
+model.add(Activation("relu"))
+
+
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit_generator(train_generator,
+                    steps_per_epoch=len(training_logs)/BATCH_SIZE,
+                    epochs=1,
+                    validation_data=test_generator,
+                    validation_steps=len(testing_logs)/BATCH_SIZE
+                    )
 
